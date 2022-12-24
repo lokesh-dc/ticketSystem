7727bfe172c5b25aa433787085aad01ca7bc12ea
lokesh-dc <lokesh.cdewanand@gmail.com>
2022-12-24 15:31:02 +0530

    strict

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbConnect = () => {
+    mongoose.set('strictQuery', false);
     return mongoose.connect("mongodb+srv://dclokesh:dcPrashant@development.9vwaikf.mongodb.net/development")
 }
 
